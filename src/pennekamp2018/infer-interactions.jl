31c5076b10745f80ea5baa252497f92a9ad2d959
Ismaël Lajaaiti <ismael.lajaaiti@gmail.com>
2025-05-22 16:34:53 +0200

    Interactions and SL

--- src/pennekamp2018/infer-interactions.jl
+++ src/pennekamp2018/infer-interactions.jl
@@ -9,9 +9,11 @@ using MixedModels
 set_theme!(theme_minimal())
 
 # Process data.
-day_start = 7 # Let some time for community to stabilise.
+day_start = 1 # Let some time for community to stabilise.
+day_end = 50
 df = DataFrame(CSV.File("data/pennekamp2018/processed-data.csv"))
 df = subset(df, :day => ByRow(>=(day_start)))
+df = subset(df, :day => ByRow(<=(day_end)))
 df = subset(
     df,
     :microcosmID => ByRow(!in([49, 229, 275, 327, 353, 359, 696, 261, 312, 406])),
@@ -36,21 +38,26 @@ species_list = df.predicted_species |> unique |> sort
 idx = Dict([sp => i for (i, sp) in enumerate(species_list)])
 S = length(species_list)
 A = zeros(S, S)
-A_normalized = zeros(S, S)
-gdf = groupby(df_duo, [:combination, :temperature])
-for a in gdf
-    a.A = (a.B_duo .- a.B_mono) ./ reverse(a.B_duo)
-    a.A_normalized = a.A ./ a.B_mono .* reverse(a.B_mono)
-    sp1, sp2 = a.predicted_species
-    A[idx[sp1], idx[sp2]] = a.A[1]
-    A[idx[sp2], idx[sp1]] = a.A[2]
-    A_normalized[idx[sp1], idx[sp2]] = a.A_normalized[1]
-    A_normalized[idx[sp2], idx[sp1]] = a.A_normalized[2]
+df_list = []
+for gdf in groupby(df_duo, :temperature)
+    A_normalized = zeros(S, S)
+    for a in groupby(gdf, :combination)
+        a.A = (a.B_duo .- a.B_mono) ./ reverse(a.B_duo)
+        a.A_normalized = a.A ./ a.B_mono .* reverse(a.B_mono)
+        sp1, sp2 = a.predicted_species
+        A[idx[sp1], idx[sp2]] = a.A[1]
+        A[idx[sp2], idx[sp1]] = a.A[2]
+        A_normalized[idx[sp1], idx[sp2]] = a.A_normalized[1]
+        A_normalized[idx[sp2], idx[sp1]] = a.A_normalized[2]
+    end
+    df_A_normalized = DataFrame(A_normalized, :auto)
+    rename!(df_A_normalized, Symbol.(species_list))
+    df_A_normalized.row = species_list
+    select!(df_A_normalized, [:row, Symbol.(species_list)...])
+    df_A_normalized.temperature = fill(gdf.temperature |> first, 6)
+    push!(df_list, df_A_normalized)
 end
+df_A_normalized = vcat(df_list...)
 
 # Prepare for saving interaction matrix as a dataframe.
-df_A_normalized = DataFrame(A_normalized, :auto)
-rename!(df_A_normalized, Symbol.(species_list))
-df_A_normalized.row = species_list
-select!(df_A_normalized, [:row, Symbol.(species_list)...])
 CSV.write("data/pennekamp2018/A_normalized.csv", df_A_normalized)
--- src/pennekamp2018/plot-data.jl
+++ src/pennekamp2018/plot-data.jl
@@ -8,10 +8,12 @@ using Distributions
 set_theme!(theme_minimal())
 
 # Process data.
-day_start = 10 # Let some time for community to stabilise.
+day_start = 1 # Let some time for community to stabilise.
+day_end = 250
 S_min = 3 # Minimal community richness, for analysis.
 df = DataFrame(CSV.File("data/pennekamp2018/processed-data.csv"))
 df = subset(df, :day => ByRow(>=(day_start)))
+df = subset(df, :day => ByRow(<=(day_end)))
 df = subset(df, :richness => ByRow(>=(S_min)))
 df_K = DataFrame(CSV.File("data/pennekamp2018/K_linear-model.csv"))
 df_avg = combine(
@@ -19,12 +21,43 @@ df_avg = combine(
     :species_biomass => mean ∘ skipmissing => :species_biomass,
 )
 df_avg = innerjoin(df_avg, df_K; on = [:predicted_species, :temperature])
+df_A = DataFrame(CSV.File("data/pennekamp2018/A_normalized.csv"))
 
 df_avg2 = combine(
     groupby(df, [:predicted_species, :temperature, :combination]),
     :species_biomass => mean ∘ skipmissing => :species_biomass,
 )
 df_avg2 = innerjoin(df_avg2, df_K; on = [:predicted_species, :temperature])
+df_avg2 = innerjoin(df_avg2, df_mono; on = [:predicted_species, :temperature])
+
+df_SL = deepcopy(df_avg2)
+subset!(df)
+rename!(df_SL, :species_biomass => :B, :K_estimated => :K)
+# df_SL = innerjoin(df_SL, df_mono; on = [:predicted_species, :temperature])
+df_SL.SL = df_SL.B ./ df_SL.B_mono
+gdf = groupby(df_SL, [:temperature, :combination])
+for a in gdf
+    sp_list = a.predicted_species
+    SL_exp_list = []
+    Ai_list = []
+    T = a.temperature |> first
+    df_A_T = subset(df_A, :temperature => ByRow(==(T)))
+    for row in eachrow(a)
+        focal_sp = row.predicted_species
+        A_received = subset(df_A, :row => ByRow(==(focal_sp)))
+        Ai = 0
+        SL_exp = 1
+        for interacting_sp in sp_list
+            SL_j = a[a.predicted_species.==interacting_sp, :SL] |> first
+            SL_exp += A_received[1, interacting_sp] * SL_j
+            Ai += A_received[1, interacting_sp]
+        end
+        push!(SL_exp_list, SL_exp)
+        push!(Ai_list, Ai)
+    end
+    a.SL_exp = SL_exp_list
+    a.Ai = Ai_list
+end
 
 # Compute arithmetic and harmonic of press perturbation intensities.
 df_kappa = DataFrame(; predicted_species = String[], kappa = Float64[])
@@ -45,10 +78,11 @@ level = 0.8 # Confidence interval for linear model.
 df_s2 = DataFrame(; ry = Float64[], s_mean = Float64[], e = [], species = [])
 for gdf in groupby(df_avg2, [:predicted_species, :combination])
     B_ref = mean(gdf.species_biomass)
-    K_ref = mean(gdf.K_estimated)
+    K_ref = mean(gdf.B_mono)
+    # K_ref = mean(gdf.K_estimated)
     ry_ref = B_ref / K_ref
-    ry = mean(gdf.species_biomass ./ gdf.K_estimated)
-    model = lm(@formula(species_biomass ~ K_estimated), gdf)
+    ry = mean(gdf.species_biomass ./ gdf.B_mono)
+    model = lm(@formula(species_biomass ~ B_mono), gdf)
     s_mean = coef(model)[2] / ry_ref
     S = unique(gdf.combination) |> first
     s_low = coeftable(model; level).cols[5][2] / ry_ref
@@ -64,7 +98,7 @@ inch = 96
 pt = 4 / 3
 cm = inch / 2.54
 width = 10cm
-fig = Figure(; size = (width, width), fontsize = 8pt);
+fig = Figure(; size = (width, 1.4width), fontsize = 8pt);
 l1 = fig[1, 1:3] = GridLayout()
 l2 = fig[2, 1:3] = GridLayout()
 ax1 = Axis(l2[1, 1]; xlabel = "Carrying capacity (μg/mL)", ylabel = "Biomass (μg/mL)")
@@ -126,16 +160,32 @@ for sp in species
     )
 end
 ry_min, ry_max = extrema(df_s2.ry)
-ry = LinRange(ry_min + 0.05, ry_max, 100)
+ry = LinRange(ry_min + 0.01, ry_max, 100)
 s_ii = 1 ./ ry
 pred_sensitivity = (s_ii .+ (1 .- s_ii) * am_on_hm_avg)
 lines!(ax3, ry, pred_sensitivity; color = :black, label = "analytical prediction")
+hlines!(1; color = :grey, linestyle = :dash)
 elems = [LineElement(), MarkerElement(; marker = :circle)]
 axislegend(ax3, elems, ["analytical\nprediction", "data"]; position = :rt)
 ax3.yreversed = true
 l1[1, 2] = Legend(fig, ax1, "Species"; rowgap = -4, tellwidth = true)
 cb = Colorbar(l2[1, 3]; limits = colorrange, colormap, label = "Temperature (°C)")
-for (label, layout) in zip(["A", "B", "C"], [l1, l2[1, 1], l2[1, 2]])
+# Last panel - interactions.
+l3 = fig[3, :] = GridLayout()
+ax = Axis(l3[1, 1]; xlabel = "SL expected", ylabel = "SL observed")
+for (i, sp) in enumerate(species_list)
+    df_sp = subset(df_SL, :predicted_species => ByRow(==(sp)))
+    scatter!(df_sp.SL_exp, df_sp.SL; alpha = 0.5)
+end
+ablines!(0, 1; color = :black, label = "1:1")
+axislegend(; position = :lt)
+ax = Axis(l3[1, 2]; xlabel = "SL expected")
+scatter!(df_SL.SL_exp, df_SL.SL; color = Float64.(df_SL.Ai), alpha = 0.5)
+ablines!(0, 1; color = :black, label = "1:1")
+colorrange = extrema(df_SL.Ai)
+cb = Colorbar(l3[1, 3]; limits = colorrange, label = "Incoming interactions")
+for (label, layout) in
+    zip(["A", "B", "C", "D", "E"], [l1, l2[1, 1], l2[1, 2], l3[1, 1], l3[1, 2]])
     Label(
         layout[1, 1, TopLeft()],
         label;
@@ -146,7 +196,8 @@ for (label, layout) in zip(["A", "B", "C"], [l1, l2[1, 1], l2[1, 2]])
 end
 fig
 
-save("figures/data.png", fig)
+save("figures/data.svg", fig)
+
 # save("figures/simulations/data.svg", fig)
 # save("figures/pennekamp2018/resistance-vs-ry.png", fig)
 # save("figures/pennekamp2018/resistance-vs-ry.svg", fig)
